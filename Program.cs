7a21e0e7116f5bba51ea36f2589f70d50d0e72d8
Brian Staal <brian@designvision.dk>
2026-03-14 21:15:59 +0100

    Missing attached files - still not working

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Google.GenAI;
+using Microsoft.Extensions.Configuration;
 using Spectre.Console;
+using System.Text;
 
 AnsiConsole.Clear();
 
@@ -15,20 +17,32 @@ IConfigurationRoot configuration = new ConfigurationBuilder()
     .Build();
 
 
-var geminiToken = configuration["GoogleGemini:Token"];
+var geminiApiKey = configuration["GoogleGemini:ApiKey"];
+var geminiModel = configuration["GoogleGemini:Model"];
 
-if (string.IsNullOrEmpty(geminiToken))
+if (string.IsNullOrEmpty(geminiApiKey) || string.IsNullOrEmpty(geminiModel))
 {
-    Console.WriteLine("Google Gemini token is not set. Please set it in the configuration & secrets.");
+    Console.WriteLine("Google Gemini ApiKey or Model is not set. Please set it in the configuration & secrets.");
     return;
 }
 
 var rootDir = AppContext.BaseDirectory;
-var instructionFile = Path.Combine(rootDir, "AppData", "instructions.txt");
+//var instructionFile = Path.Combine(rootDir, "AppData", "instructions.txt");
 var pdfFile = Path.Combine(rootDir, "AppData", "Beierholm.pdf");
-var outputFormatFile = Path.Combine(rootDir, "AppData", "output.json");
+var outputSchema = Path.Combine(rootDir, "AppData", "output.schema.json");
 
-var instructionContent = File.ReadAllText(instructionFile);
+//var sb = new StringBuilder();
+//sb.Append(System.IO.File.ReadAllText(instructionFile));
+
+// Create a new Gemini client
+var client = new Client(apiKey:geminiApiKey);
+var pdfFileRef = await client.Files.UploadAsync(filePath: pdfFile);
+var outputRef = await client.Files.UploadAsync(filePath: outputSchema);
+
+var response = await client.Models.GenerateContentAsync(model: geminiModel, contents: "Extract data from Beierholm.pdf by the given output.schema.json, and return a json");
+
+var responseText = response.Text;
+Console.WriteLine(responseText);
 
 // Todo: Add the pfdFile + outputFormatFile gemini and give the instructionContent as the system prompt. Then get the response and write it to the console.
 
